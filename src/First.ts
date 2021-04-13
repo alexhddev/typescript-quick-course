2eac012ed63a88ef3bb4ce3031c8d1200da65a1f
barosanuemailtest <barosanu.email.test@gmail.com>
2021-04-13 09:13:33 +0300

    Initial

---
+++ src/First.ts
@@ -0,0 +1,45 @@
+
+
+interface Person {
+    firstName: string,
+    lastName: string,
+    job?: job
+    isVisitor?: boolean
+}
+
+type job = 'Engineer' | 'Programmer'
+
+
+function generateEmail(input: Person, force?: boolean): string | undefined {
+    if (input.isVisitor && !force) {
+        return undefined
+    } else {
+        return `${input.firstName}.${input.lastName}@email.com`
+    }
+}
+
+function isPerson(potentialPerson: any): boolean {
+    if ('firstName' in potentialPerson &&
+        'lastName' in potentialPerson) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function printEmailIfPerson(potentialPerson: any): void {
+    if (isPerson(potentialPerson)) {
+        console.log(generateEmail(potentialPerson))
+    } else {
+        console.log('Input is not a person')
+    }
+}
+
+printEmailIfPerson({
+    firstName: 'John',
+    lastNames: 'Doe'
+})
+
+async function someAsyn() {
+    return 'async'
+}
