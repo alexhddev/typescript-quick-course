95fcdc7bc5e179ca15ced800bc44fb02b6724b5d
barosanuemailtest <barosanu.email.test@gmail.com>
2021-04-13 09:39:29 +0300

    Class practice

---
+++ src/Server.ts
@@ -0,0 +1,21 @@
+
+
+
+class Server {
+
+    port: number;
+    address: string;
+
+    constructor(port: number, address: string){
+        this.port = port;
+        this.address = address;
+    }
+
+    startServer(){
+        console.log(`Starting server at: ${this.address}: ${this.port}`)
+    }
+
+}
+
+const someServer = new Server(8080, 'localhost');
+someServer.startServer();
