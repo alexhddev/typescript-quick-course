f3dcd9da7ae8ca16e7627615aceef374e7753dcd
barosanuemailtest <barosanu.email.test@gmail.com>
2021-04-13 16:04:01 +0300

    Decorators

--- src/Server.ts
+++ src/Server.ts
@@ -1,4 +1,13 @@
-import {  Comp1 } from '@components/Comp1'
+
+function logInvocation(target: Object, propertyKey: string, descriptor: PropertyDescriptor){
+    const className = target.constructor.name;
+    let originalMethod = descriptor.value;
+    descriptor.value = async function(...args: any[]) {
+        console.log(`${className}#${propertyKey} called with ${JSON.stringify(args)}`)
+        const result = await originalMethod.apply(this, args);
+        console.log(`${className}#${propertyKey} returned ${JSON.stringify(result)}`)
+    }
+}
 
 
 export interface IServer {
@@ -11,7 +20,6 @@ class Server implements IServer{
 
     public port: number;
     public address: string;
-    public comp1 = new Comp1;
 
     constructor(port: number, address: string){
         this.port = port;
@@ -19,14 +27,15 @@ class Server implements IServer{
     }
 
     async startServer(){
-        const data = await this.getData();
+        const data = await this.getData(123);
         console.log(`Starting server at: ${this.address}: ${this.port}`)
     }
 
     stopServer(): void {}
 
-    async getData(): Promise<string>{
-        return '{}'
+    @logInvocation
+    async getData(id: number): Promise<string>{
+        return 'some SPecial Data'
     }
 
 }
